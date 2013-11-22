6c3d71f962edf795cbb4ff43de332693dc646faf
Navid Norouzi <navid.norouzi@lmnsolutions.com>
2013-11-22 13:14:33 -0500

    Added in the Angular services and other initial infrastructure changes

---
+++ public/assets/js/app/services/feedsService.js
@@ -0,0 +1,20 @@
+'use strict';
+/*
+ * Feeds Service
+ *
+ * Returning promises, allowing the Controller to handle success and error responses appropriately
+ *
+ */
+vipApp.factory('$feedsService', function ($http, $appProperties) {
+
+    return {
+        getData: function () {
+            return $http.get($appProperties.servicesPath + "/feedsMockService.html");
+        },
+        postService: function () {
+            var postData = {key: "One", value: "1"};
+
+            return $http.post("http://www.google.com", data);
+        }
+    };
+});
