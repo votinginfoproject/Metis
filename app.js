4254471cdfaf151b04e53450cd617b9658bfe0ed
Brett Antonides <brett.antonides@lmnsolutions.com>
2013-11-27 12:11:04 -0500

    Refactored Authentication into Separate Files
    * Moved authentication code into separate file to make code cleaner and more modular

--- app.js
+++ app.js
@@ -7,14 +7,10 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var passport = require('passport');
-var CrowdStrategy = require('passport-atlassian-crowd').Strategy;
-var _ = require('underscore');
 var auth = require('./auth');
 
 var app = express();
 
-//logged in user profiles
-var profiles = [];
 
 // all environments
 app.use(express.favicon(config.web.favicon));
@@ -34,48 +30,14 @@ if ('development' == app.get('env')) {
     app.use(express.errorHandler());
 }
 
-//user serialization
-passport.serializeUser(function (user, done) {
-    done(null, user.username);
-});
-
-passport.deserializeUser(function (username, done) {
-    var user = _.find(profiles, function (user) {
-        return user.username == username;
-    });
-    if (user === undefined) {
-        done(new Error("No user with username '" + username + "' found."));
-    }
-    else {
-        done(null, user);
-    }
-});
-
-passport.use(new CrowdStrategy({
-        crowdServer: config.crowd.server,
-        crowdApplication: config.crowd.application,
-        crowdApplicationPassword: config.crowd.apppass,
-        retrieveGroupMemberships: config.crowd.retrieveGroups
-    },
-    function (userprofile, done) {
-        process.nextTick(function () {
-            var exists = _.any(profiles, function (user) {
-                return user.id == userprofile.id;
-            });
-            if (!exists) {
-                profiles.push(userprofile);
-            }
-            return done(null, userprofile);
-        })
-    }
-));
+//user authentication
+auth.authSetup(config, passport);
 
 app.post('/login',
-    passport.authenticate('atlassian-crowd', { failureRedirect: '/loginfail'}),
+    auth.authLogin,
     function(req, res) {
-        res.redirect('/');
-    });
-
+        res.redirect('/loggedin');
+});
 
 http.createServer(app).listen(config.web.port, function () {
     console.log('Express server listening on port ' + config.web.port);
--- auth.js
+++ auth.js
@@ -1,11 +1,64 @@
 /**
  * Created by bantonides on 11/22/13.
  */
-var authCheck = function(req, res, next) {
+
+var CrowdStrategy = require('passport-atlassian-crowd').Strategy;
+var _ = require('underscore');
+var passport;
+
+//logged in user profiles
+var profiles = [];
+
+var setup = function(config, pp) {
+    passport = pp;
+
+    passport.serializeUser(function (user, done) {
+        done(null, user.username);
+    });
+
+    passport.deserializeUser(function (username, done) {
+        var user = _.find(profiles, function (user) {
+            return user.username == username;
+        });
+        if (user === undefined) {
+            done(new Error("No user with username '" + username + "' found."));
+        }
+        else {
+            done(null, user);
+        }
+    });
+
+    passport.use(new CrowdStrategy({
+            crowdServer: config.crowd.server,
+            crowdApplication: config.crowd.application,
+            crowdApplicationPassword: config.crowd.apppass,
+            retrieveGroupMemberships: config.crowd.retrieveGroups
+        },
+        function (userprofile, done) {
+            process.nextTick(function () {
+                var exists = _.any(profiles, function (user) {
+                    return user.id == userprofile.id;
+                });
+                if (!exists) {
+                    profiles.push(userprofile);
+                }
+                return done(null, userprofile);
+            })
+        }
+    ));
+};
+
+var login = function (req, options) {
+    passport.authenticate('atlassian-crowd', { failureRedirect: '/loginfail'});
+};
+
+var check = function(req, res, next) {
   if (!req.isAuthenticated())
     res.send(401);
   else
     next();
 };
 
-exports.authCheck = authCheck;
+exports.authSetup = setup;
+exports.authLogin = login;
+exports.authCheck = check;
