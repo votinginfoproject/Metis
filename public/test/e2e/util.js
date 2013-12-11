4cec91fbf646c22294428a7ae3476425589c20ca
Ryan Cartier <rcartier13@gmail.com>
2013-12-11 17:25:14 -0500

    removed unused function from util.js

--- public/test/e2e/util.js
+++ public/test/e2e/util.js
@@ -26,13 +26,3 @@ e2eLoadPage = function ($url) {
   browser().navigateTo($url);
   sleep(0.5);
 }
-
-e2eValueofRepeater = function ($name, $binding) {
-  // Ugly code on how to get a value from a future
-  // Gets the future from the first date in the table
-  var val = repeater($name).column($binding);
-  // Executes that future populating its value function
-  val.execute(function() {});
-  // Sets a variable to that futures value
-  return val.value;
-};
