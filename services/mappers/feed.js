a67057b095395f0083d4b4a22185d71df374c153
Brett Antonides <brett.antonides@lmnsolutions.com>
2013-12-16 12:26:18 -0500

    Added additional services for returning feed details
    * Added the following services:
        * election/contests
        * polling
        * history
        * contests

--- services/mappers/feed.js
+++ services/mappers/feed.js
@@ -74,7 +74,118 @@ var mapElection = function(path, data) {
   };
 };
 
+var mapElectionContests = function(path, data) {
+  return [
+    {
+      id: 320004744,
+      type: 'General',
+      title: 'US President'
+    },
+    {
+      id: 60001,
+      type: 'General',
+      title: 'State Treasurer'
+    },
+    {
+      id: 60006,
+      type: 'Referendum',
+      title: 'Proposition 37'
+    },
+    {
+      id: 60007,
+      type: 'Judge Retention',
+      title: 'Should Judge Carlton Smith be retained?'
+    }
+  ];
+};
+
+var mapPolling = function(path, data) {
+  return [
+    {
+      element_type: 'Localities',
+      amount: 100,
+      complete_pct: 6,
+      error_count: 0
+    },
+    {
+      element_type: 'Electoral Districts',
+      amount: 976,
+      complete_pct: 21,
+      error_count: 888
+    },
+    {
+      element_type: 'Precincts',
+      amount: 2756,
+      complete_pct: 94,
+      error_count: 206
+    }
+  ];
+};
+
+var mapContests = function(path, data) {
+  return [
+    {
+      element_type: 'Contests',
+      amount: 976,
+      complete_pct: 0,
+      error_count: 14
+    },
+    {
+      element_type: 'Ballots',
+      amount: 976,
+      complete_pct: 38,
+      error_count: 140
+    },
+    {
+      element_type: 'Candidates',
+      amount: 1761,
+      complete_pct: 91,
+      error_count: 1156
+    }
+  ];
+};
+
+var mapResults = function(path, data) {
+  return [
+    {
+      element_type: 'Contest Results',
+      amount: 0,
+      complete_pct: 0,
+      error_count: 0
+    },
+    {
+      element_type: 'Ballot Line Results',
+      amount: 10,
+      complete_pct: 100,
+      error_count: 2
+    }
+  ];
+};
+
+var mapHistory = function(path, data) {
+  return [
+    {
+      date: moment(new Date()).format('YYYY-MM-DD'),
+      events: [
+        {
+          time: moment('1:11 PM', 'h:mm A').format('h:mm A'),
+          description: 'Processing'
+        },
+        {
+          time: moment('2:13 PM', 'h:mm A').format('h:mm A'),
+          description: 'Ready'
+        }
+      ]
+    }
+  ];
+};
+
 exports.mapFeed = mapFeed;
 exports.mapFeedOverview = mapOverview;
 exports.mapSource = mapSource;
 exports.mapElection = mapElection;
+exports.mapElectionContest = mapElectionContests;
+exports.mapPollingSummary = mapPolling;
+exports.mapContests = mapContests;
+exports.mapResults = mapResults;
+exports.mapHistory = mapHistory;
