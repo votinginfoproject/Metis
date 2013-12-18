88d797a6e109783f56cfae34dd8245eed6d765ec
Ryan Cartier <rcartier13@gmail.com>
2013-12-18 09:49:51 -0500

    changed the way that the MockE2E works and now no longer is included when you aren’t running tests.

--- public/assets/js/app/app.js
+++ public/assets/js/app/app.js
@@ -4,13 +4,17 @@
  *
  */
 
-var isTesting = false;
+var isTesting = true;
 
 // Comment in if want to disable all "debug" logging
 //debug.setLevel(0);
 
 // VIP app module with its dependencies
-var vipApp = angular.module('vipApp', ['ngTable', 'ngRoute', 'ngCookies', 'ngMockE2E']);
+var vipApp;
+if(isTesting)
+  vipApp = angular.module('vipApp', ['ngTable', 'ngRoute', 'ngCookies', 'ngMockE2E']);
+else
+  vipApp = angular.module('vipApp', ['ngTable', 'ngRoute', 'ngCookies']);
 
 // Constants
 vipApp.constant('$appProperties', {
@@ -58,92 +62,92 @@ vipApp.config(['$routeProvider', '$appProperties', '$httpProvider',
     });
 
     $routeProvider.when('/template/feed', {
-        templateUrl: $appProperties.contextRoot + '/app/partials/templates/feed.html'
+      templateUrl: $appProperties.contextRoot + '/app/partials/templates/feed.html'
     });
 
     $routeProvider.when('/template/source', {
-        templateUrl: $appProperties.contextRoot + '/app/partials/templates/source.html'
+      templateUrl: $appProperties.contextRoot + '/app/partials/templates/source.html'
     });
 
     $routeProvider.when('/template/election', {
-        templateUrl: $appProperties.contextRoot + '/app/partials/templates/election.html'
+      templateUrl: $appProperties.contextRoot + '/app/partials/templates/election.html'
     });
 
     $routeProvider.when('/template/state', {
-        templateUrl: $appProperties.contextRoot + '/app/partials/templates/state.html'
+      templateUrl: $appProperties.contextRoot + '/app/partials/templates/state.html'
     });
 
     $routeProvider.when('/template/locality', {
-        templateUrl: $appProperties.contextRoot + '/app/partials/templates/locality.html'
+      templateUrl: $appProperties.contextRoot + '/app/partials/templates/locality.html'
     });
 
     $routeProvider.when('/template/precinct', {
-        templateUrl: $appProperties.contextRoot + '/app/partials/templates/precinct.html'
+      templateUrl: $appProperties.contextRoot + '/app/partials/templates/precinct.html'
     });
 
     $routeProvider.when('/template/precinct-split', {
-        templateUrl: $appProperties.contextRoot + '/app/partials/templates/precinct-split.html'
+      templateUrl: $appProperties.contextRoot + '/app/partials/templates/precinct-split.html'
     });
 
     $routeProvider.when('/template/election-administration', {
-        templateUrl: $appProperties.contextRoot + '/app/partials/templates/election-administration.html'
+      templateUrl: $appProperties.contextRoot + '/app/partials/templates/election-administration.html'
     });
 
     $routeProvider.when('/template/early-vote-site', {
-        templateUrl: $appProperties.contextRoot + '/app/partials/templates/early-vote-site.html'
+      templateUrl: $appProperties.contextRoot + '/app/partials/templates/early-vote-site.html'
     });
 
     $routeProvider.when('/template/polling-location', {
-        templateUrl: $appProperties.contextRoot + '/app/partials/templates/polling-location.html'
+      templateUrl: $appProperties.contextRoot + '/app/partials/templates/polling-location.html'
     });
 
     $routeProvider.when('/template/contests', {
-        templateUrl: $appProperties.contextRoot + '/app/partials/templates/contests.html'
+      templateUrl: $appProperties.contextRoot + '/app/partials/templates/contests.html'
     });
 
     $routeProvider.when('/template/contest', {
-        templateUrl: $appProperties.contextRoot + '/app/partials/templates/contest.html'
+      templateUrl: $appProperties.contextRoot + '/app/partials/templates/contest.html'
     });
 
     $routeProvider.when('/template/ballot', {
-        templateUrl: $appProperties.contextRoot + '/app/partials/templates/ballot.html'
+      templateUrl: $appProperties.contextRoot + '/app/partials/templates/ballot.html'
     });
 
     $routeProvider.when('/template/candidate', {
-        templateUrl: $appProperties.contextRoot + '/app/partials/templates/candidate.html'
+      templateUrl: $appProperties.contextRoot + '/app/partials/templates/candidate.html'
     });
 
     $routeProvider.when('/template/electoral-district', {
-        templateUrl: $appProperties.contextRoot + '/app/partials/templates/electoral-district.html'
+      templateUrl: $appProperties.contextRoot + '/app/partials/templates/electoral-district.html'
     });
 
     $routeProvider.when('/template/contest-result', {
-        templateUrl: $appProperties.contextRoot + '/app/partials/templates/contest-result.html'
+      templateUrl: $appProperties.contextRoot + '/app/partials/templates/contest-result.html'
     });
 
     $routeProvider.when('/template/ballot-line-result', {
-        templateUrl: $appProperties.contextRoot + '/app/partials/templates/ballot-line-result.html'
+      templateUrl: $appProperties.contextRoot + '/app/partials/templates/ballot-line-result.html'
     });
 
     $routeProvider.when('/template/results', {
-        templateUrl: $appProperties.contextRoot + '/app/partials/templates/results.html'
+      templateUrl: $appProperties.contextRoot + '/app/partials/templates/results.html'
     });
 
     $routeProvider.when('/template/errors', {
-        templateUrl: $appProperties.contextRoot + '/app/partials/templates/errors.html'
+      templateUrl: $appProperties.contextRoot + '/app/partials/templates/errors.html'
     });
 
     $routeProvider.when('/template/search-results', {
-        templateUrl: $appProperties.contextRoot + '/app/partials/templates/search-results.html'
+      templateUrl: $appProperties.contextRoot + '/app/partials/templates/search-results.html'
     });
 
     $routeProvider.when('/template/grid', {
-        templateUrl: $appProperties.contextRoot + '/app/partials/templates/grid.html'
+      templateUrl: $appProperties.contextRoot + '/app/partials/templates/grid.html'
     });
 
     $routeProvider.when('/profile', {
-        templateUrl: $appProperties.contextRoot + '/app/partials/profile.html',
-        controller: 'ProfileCtrl'
+      templateUrl: $appProperties.contextRoot + '/app/partials/profile.html',
+      controller: 'ProfileCtrl'
     });
 
     $routeProvider.when('/styleguide', {
@@ -194,38 +198,36 @@ vipApp.config(['$routeProvider', '$appProperties', '$httpProvider',
  */
 vipApp.run(function ($rootScope, $appService, $location, $httpBackend, $appProperties) {
 
+
   $rootScope.pageHeader = {};
   $rootScope.user = null;
 
-  var feeds = [
-    {date: '2011-10-12', state: 'Ohio', type: 'Federal', status: 'Undetermined'},
-    {date: '2011-10-11', state: 'Florida', type: 'State', status: 'determined'},
-    {date: '2011-10-10', state: 'Delaware', type: 'Federal', status: 'Undetermined'},
-    {date: '2011-10-25', state: 'North Carolina', type: 'State', status: 'determined'},
-    {date: '2011-08-15', state: 'South Carolina', type: 'State', status: 'Undetermined'},
-    {date: '2012-10-25', state: 'Virginia', type: 'Federal', status: 'Undetermined'},
-    {date: '2013-12-16', state: 'Georgia', type: 'State', status: 'determined'},
-    {date: '2011-09-08', state: 'Texas', type: 'Federal', status: 'Undetermined'},
-    {date: '2011-07-10', state: 'New York', type: 'State', status: 'Undetermined'},
-    {date: '2011-08-13', state: 'California', type: 'Federal', status: 'Undetermined'},
-    {date: '2011-11-01', state: 'Vermont', type: 'Federal', status: 'determined'},
-    {date: '2011-10-02', state: 'West Virginia', type: 'Federal', status: 'Undetermined'}
-  ]
-
-  $httpBackend.whenGET($appProperties.mockServicesPath + "/adminMockService.html").passThrough();
-  $httpBackend.whenGET($appProperties.servicesPath + "/getUser").passThrough();
-  $httpBackend.whenGET($appProperties.mockServicesPath + "/referenceDataMockService.html").passThrough();
-  $httpBackend.whenGET($appProperties.mockServicesPath + "/homeMockService.html").passThrough();
-  $httpBackend.whenGET($appProperties.servicesPath + "/xxxxxxxxxx").passThrough();
-  $httpBackend.whenGET($appProperties.mockServicesPath + "/profileMockService.html").passThrough();
-
   if(isTesting)
+  {
+    var feeds = [
+      {date: '2011-10-12', state: 'Ohio', type: 'Federal', status: 'Undetermined'},
+      {date: '2011-10-11', state: 'Florida', type: 'State', status: 'determined'},
+      {date: '2011-10-10', state: 'Delaware', type: 'Federal', status: 'Undetermined'},
+      {date: '2011-10-25', state: 'North Carolina', type: 'State', status: 'determined'},
+      {date: '2011-08-15', state: 'South Carolina', type: 'State', status: 'Undetermined'},
+      {date: '2012-10-25', state: 'Virginia', type: 'Federal', status: 'Undetermined'},
+      {date: '2013-12-16', state: 'Georgia', type: 'State', status: 'determined'},
+      {date: '2011-09-08', state: 'Texas', type: 'Federal', status: 'Undetermined'},
+      {date: '2011-07-10', state: 'New York', type: 'State', status: 'Undetermined'},
+      {date: '2011-08-13', state: 'California', type: 'Federal', status: 'Undetermined'},
+      {date: '2011-11-01', state: 'Vermont', type: 'Federal', status: 'determined'},
+      {date: '2011-10-02', state: 'West Virginia', type: 'Federal', status: 'Undetermined'}
+    ]
+
+    $httpBackend.whenGET($appProperties.mockServicesPath + "/adminMockService.html").passThrough();
+    $httpBackend.whenGET($appProperties.servicesPath + "/getUser").passThrough();
+    $httpBackend.whenGET($appProperties.mockServicesPath + "/referenceDataMockService.html").passThrough();
+    $httpBackend.whenGET($appProperties.mockServicesPath + "/homeMockService.html").passThrough();
+    $httpBackend.whenGET($appProperties.servicesPath + "/xxxxxxxxxx").passThrough();
+    $httpBackend.whenGET($appProperties.mockServicesPath + "/profileMockService.html").passThrough();
     $httpBackend.whenGET($appProperties.servicesPath + "/feeds").respond(feeds);
-  else
-    $httpBackend.whenGET($appProperties.servicesPath + "/feeds").passThrough();
-
-  $httpBackend.whenGET(/partials\/.*/).passThrough();
-
+    $httpBackend.whenGET(/partials\/.*/).passThrough();
+  }
   /*
    * Sets PageHeader values
    *
