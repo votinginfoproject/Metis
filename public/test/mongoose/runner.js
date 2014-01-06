5c6c401409a8b576717b98d000de3cdbab7c231f
Ryan Cartier <rcartier13@gmail.com>
2014-01-06 10:56:00 -0500

    finished the functionality for the mongoose test runner, now properly starts and ends.

--- public/test/mongoose/runner.js
+++ public/test/mongoose/runner.js
@@ -3,8 +3,8 @@
  */
 
 var failedCount = 0;
-var total = 0;
-var ballots, ballotResponse, ballotLineResponse;
+var total = 4;
+var savedData = [];
 
 //app configuration
 var config = require('../../../config');
@@ -12,6 +12,8 @@ var config = require('../../../config');
 //database setup
 var mongoose = require('mongoose');
 var daoSchemas = require('../../../dao/schemas');
+var tests = require('./tests');
+
 
 mongoose.connect(config.mongoose.testConnectionString);
 var db = mongoose.connection;
@@ -25,7 +27,7 @@ db.once('open', function callback(){
 
   console.log("Testing Schemas: ");
   runTests(daoSchemas.models, function() {
-    console.log((total-failedCount) + " of " + total + " passed.");
+    console.log(total - failedCount + " out of " + total + " passed!");
     mongoose.disconnect();
   });
 });
@@ -37,12 +39,11 @@ function acquire(description, ifPassed) {
     failedCount++;
   } else {
     console.log(description + " test Passed!");
-  }
-  total++;
+  };
 };
 
 function saveSchemas(models) {
-  ballots = new models.Ballot({
+  savedData['ballots'] = new models.Ballot({
     elementId: 1,
     referendumId: 2,
     candidates: [{id: 3, sortOrder: 0}, {id:4, sortOrder: 1}],
@@ -50,16 +51,18 @@ function saveSchemas(models) {
     writeIn: false,
     imageUrl: 'FakeUrl.edu'
   });
-  ballots.save();
 
-  ballotResponse = new models.BallotResponse({
+  savedData['ballots'].save();
+
+  savedData['ballotResponse'] = new models.BallotResponse({
     elementId: 1,
     test: 'something',
     sortOrder: 2
   });
-  ballotResponse.save();
 
-  ballotLineResponse = new models.BallotLineResult({
+  savedData['ballotResponse'].save();
+
+  savedData['ballotLineResponse'] = new models.BallotLineResult({
     elementId: 1, //required
     contestId: 2,
     jurisdictionId: 3,
@@ -70,32 +73,49 @@ function saveSchemas(models) {
     victorious: false,
     certification: 'something'
   });
-  ballotLineResponse.save();
+
+  savedData['ballotLineResponse'].save();
+
+  var simpleAddress = {
+    locationName: 'Place',
+    line1: 'blah',
+    line2: '',
+    line3: '',
+    city: 'Reston',
+    state: 'VA',
+    zip: '20202'
+  };
+
+  savedData['candidate'] = new models.Candidate({
+    elementId: 1, //required
+    name: 'something',
+    party: 'Tea',
+    candidateUrl: 'www.awesome.com',
+    biography: 'is a person',
+    phone: '555-555-5555',
+    photoUrl: 'www.imgur.com',
+    filedMailingAddress: simpleAddress,
+    email: 'booyah@yahoo',
+    sortOrder: 2
+  });
+
+  savedData['candidate'].save();
 };
 
-function runTests(models, cb) {
-  var isPassing;
-  models.Ballot.find({}, function(err, foundVal) {
-    isPassing = true;
-    if(err || foundVal[0].elementId !== ballots.elementId)
-      isPassing = false;
-    acquire('Ballot Schema Test', isPassing);
-  });
+function runTests(models, callback) {
 
-  models.BallotResponse.find({}, function(err, foundVal) {
-    isPassing = true;
-    if(err || foundVal[0].elementId !== ballotResponse.elementId)
-      isPassing = false;
-    acquire('Ballot Response Schema Test', isPassing);
-  });
+  var count = 0;
+  function countUp(description, isPassing) {
+    acquire(description, isPassing);
+    ++count;
+    if(count === total)
+      callback();
+  };
 
-  models.BallotLineResult.find({}, function(err, foundVal) {
-    isPassing = true;
-    if(err || foundVal[0].elementId !== ballotLineResponse.elementId)
-      isPassing = false;
-    acquire('Ballot Line Result Schema Test', isPassing);
-    cb();
-  });
+  tests.ballotTest(models, savedData['ballots'], countUp);
+  tests.ballotResponseTest(models, savedData['ballotResponse'], countUp);
+  tests.ballotLineResult(models, savedData['ballotLineResponse'], countUp);
+  tests.candidate(models, savedData['candidate'], countUp);
 };
 
 
