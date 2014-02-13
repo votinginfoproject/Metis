6761bad19246b9bc5d20788973b5c705ea53c336
NoBo <NoBo.worldwide>
2014-02-13 07:09:17 -0500

    removed test code from rules engine. uniqueIdRuleToo now wired up for saves. next step: modify UniqueIdRuleToo save strategy so that it follows other /impl rule formats

--- feed-processor/rule/impl/uniqueIdToo.js
+++ feed-processor/rule/impl/uniqueIdToo.js
@@ -8,8 +8,11 @@ var violation = require('../ruleViolation');
 var when = require('when');
 var deferred = when.defer();
 
-var evaluateUniqueId = function(feedId, constraintSet, ruleDef){
+var rule = null;
+
+var evaluateUniqueId = function(feedId, constraintSet, ruleDefinition){
   var when = require('when');
+  rule = ruleDefinition;
 
   //console.log("feedId", feedId);
   var finds = [];
@@ -20,7 +23,7 @@ var evaluateUniqueId = function(feedId, constraintSet, ruleDef){
     }, this);
 
     when.all(finds)
-      .then(processQueryResults.bind(this), errorHandler);
+      .then(processQueryResults, errorHandler);
 
   return deferred.promise;
 }
@@ -43,93 +46,60 @@ function processQueryResults(foundDocs) {
       idCounts[id] = { count: 0, errorModel: [] };
     }
     idCounts[id].count++;
-    idCounts[id].errorModel.push( { model: doc.constructor.Error, ref: doc._id });
+    idCounts[id].errorModel.push( { model: doc.constructor.Error, _feed: doc._feed, _ref: doc._id });
   });
 
-  storeErrors(filterDuplicates(idCounts), this);
+  storeErrors(filterDuplicates(idCounts));
   this.complete = true;
   console.log('processQueryResults complete');
 }
 
-  function filterDuplicates(idCounts) {
-    console.log('Filtering dulicates.');
-    var duplicateIds = Object.keys(idCounts).filter(function(key) {
-      return idCounts[key].count > 1;
-    });
+function filterDuplicates(idCounts) {
+  console.log('Filtering duplicates.');
+  var duplicateIds = Object.keys(idCounts).filter(function(key) {
+    return idCounts[key].count > 1;
+  });
 
-    var duplicates = [];
+  var duplicates = [];
 
-    duplicateIds.forEach(function(id) {
-      console.log('dupe found', id);
-      idCounts[id].id = id;
-      duplicates.push(idCounts[id]);
-    });
+  duplicateIds.forEach(function(id) {
+    console.log('dupe found', id);
+    idCounts[id].id = id;
+    duplicates.push(idCounts[id]);
+  });
 
-    return duplicates;
-  }
+  return duplicates;
+}
 
-function storeErrors(dupes, feedId, context) {
+function storeErrors(dupes, feedId) {
   console.log('Storing errors.');
   var savePromises = [];
 
   dupes.forEach(function(dupe) {
     dupe.errorModel.forEach(function(errModel) {
-      savePromises.push(saveError(context, errModel, dupe.id, feedId));
+      savePromises.push(saveError(errModel, dupe.id, feedId));
     });
   });
 
-  //when.all(savePromises).then(context.callback, errorHandler).then(function(){deferred.resolve({ isViolated: false });});
   when.all(savePromises).then(function(){ console.log('resolving'); deferred.resolve({ isViolated: false });});
 }
 
 
-  function saveError(context, errorModel, id) {
-    //context.refCount++;
-    console.log('Saving error: ');
-
+  function saveError(errorModel, id) {
+    console.log('Unique Id Rule Violation: ');
     //errorModel.model.create({
-// createHandler.bind(context));
-    console.log('id=', id);
-    //console.log(errorModel);
-    //console.log(context);
-    /*console.log({
-      severityCode: context.rule.severityCode,
-      severityText: context.rule.severityText,
-      errorCode: context.rule.errorCode,
-      title: context.rule.title,
-      details: context.rule.description,
+    console.log({
+      severityCode: rule.severityCode,
+      severityText: rule.severityText,
+      errorCode: rule.errorCode,
+      title: rule.title,
+      details: rule.details,
       textualReference: 'id = ' + id,
       refElementId: id,
-      _ref: errorModel.ref,
-      _feed: feedId
-    });*/
-
+      _ref: errorModel._ref,
+      _feed: errorModel._feed
+    });
   }
 
-/*
- function saveError(context, errorModel, id, feedId) {
- return errorModel.model.create({
- severityCode: context.rule.severityCode,
- severityText: context.rule.severityText,
- errorCode: context.rule.errorCode,
- title: context.rule.title,
- details: context.rule.description,
- textualReference: 'id = ' + id,
- refElementId: id,
- _ref: errorModel.ref,
- _feed: feedId
- });
- }
- */
-
-  function createHandler(err) {
-    this.refCount--;
-    errorHandler(err);
-    if (this.complete && this.refCount <= 0) {
-      console.log('Calling completion callback.');
-      this.callback();
-    }
-
-}
 
 exports.evaluate = evaluateUniqueId;
--- feed-processor/rule/rulesEngine.js
+++ feed-processor/rule/rulesEngine.js
@@ -1,17 +1,14 @@
 
-var metisRule = require('./metisrule');
-var ruleList = require('./rulelist');
 
 var async = require('async');
-var config = require('../../config');
-var mongoose = require('mongoose');
-var schemas = require('../../dao/schemas');
+var metisRuleHandler = require('./metisrule');
+var ruleList = require('./rulelist');
 
+var ruleHandler = new metisRuleHandler();
 var rules = [];
-var ruleHandler = new metisRule();
 
 
-var processRules = function(vipFeedId){
+function processRules(vipFeedId){
   console.log('initializing rules..');
   async.each(ruleList, loadRule, function(err){
     console.log(rules.length, 'Metis rules loaded and staged for analysis');
@@ -38,23 +35,4 @@ var endSession = function(){
   process.exit();
 }
 
-/*  for external testing */
-
-function connectMongo(connectionString, next) {
-  mongoose.connect(connectionString);
-  db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'MongoDB connection error: '));
-  db.once('open', function callback(){
-    console.log("initialized VIP database via Mongoose");
-    next();
-  });
-}
-
-function executeProg(vipFeedId) {
-  schemas.initSchemas(mongoose);
-  connectMongo(config.mongoose.connectionString, function () {
-    processRules(vipFeedId);
-  });
-}
-
-executeProg(process.argv[2]);
+exports.processRules = processRules;
