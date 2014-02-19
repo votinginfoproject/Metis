22d899db3137513efcbf73c30ba2c4abcfd109aa
Ryan Cartier <ryan.cartier@lmnsolutions.com>
2014-02-19 10:23:29 -0500

    updated to run the overview processor until it is officially hooked up to the feed processor

--- public/test/RunFunctional.sh
+++ public/test/RunFunctional.sh
@@ -9,5 +9,8 @@ cd ../../feed-processor/
 #run processor.js
 node processor.js ../public/test/Mocked\ DB/test_feed.xml
 
+# Run the overview process
+node overview/overview-processor.js
+
 # Opens chrome and navigates to tests
 open /Applications/Google\ Chrome.app "http://localhost:4000/test/e2e/runner.html"
