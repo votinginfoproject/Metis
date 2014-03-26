27f63964d0e107346263158a3b4514f695b95e28
Navid Norouzi <navid.norouzi@lmnsolutions.com>
2014-03-26 02:08:47 -0400

    fix for when process is exited during an aggregation error - setting the exit code to -1 which will allow the parent process to not write over the status code that gets sets when the error occurs. Also fixed a bug with the Error feeds continueing to countdown during the next processing

--- services/processManager.js
+++ services/processManager.js
@@ -96,6 +96,9 @@ function startFileProcessing(filename){
     var pid = fileProcessing.pid + "";
 
     // if there was an error in the child process mark the feed as such
+    // code 0 means everything was ok
+    // any code below 0 is our own code for things that go wrong, so we won't check those as if we are
+    // setting the codes for exit, we will also update the status of the feed at that point in time
     if(code>0 && pIdsAndFeedIds[pid] != undefined ){
 
       schemas.models.Feed.update({_id: pIdsAndFeedIds[pid]}, { feedStatus: 'Errored', complete: false, failed: true },
