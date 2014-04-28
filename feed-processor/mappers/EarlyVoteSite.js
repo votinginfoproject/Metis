8dd6b1274ae0fb4802c2d945adadb85e857ffb2b
Ryan Cartier <ryan.cartier@lmnsolutions.com>
2014-04-28 14:42:13 -0400

    fixed some schemas that still had ids as integers fixed the ui so that when customballots have 0 errors the link turns green same with street segments in precinct splits / precinct added feeds + upload folders to the .gitignore list

--- feed-processor/mappers/EarlyVoteSite.js
+++ feed-processor/mappers/EarlyVoteSite.js
@@ -24,7 +24,17 @@ EarlyVoteSite.prototype.mapXml3_0 = function (earlyVoteSite) {
 };
 
 EarlyVoteSite.prototype.mapXml5_0 = function (earlyVoteSite) {
-  this.mapXml3_0(earlyVoteSite);
+  this.model = new this.models.EarlyVoteSite({
+    elementId: earlyVoteSite.$.id,     //required
+    name: earlyVoteSite.name,
+    address: this.mapSimpleAddress(earlyVoteSite.address),
+    directions: earlyVoteSite.directions,
+    voterServices: earlyVoteSite.voter_services,
+    startDate: earlyVoteSite.start_date,
+    endDate: earlyVoteSite.end_date,
+    daysTimesOpen: earlyVoteSite.days_times_open,
+    _feed: this.feedId
+  });
 };
 
 EarlyVoteSite.prototype.mapCsv = function (earlyVoteSite) {
