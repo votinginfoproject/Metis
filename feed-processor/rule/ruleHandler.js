361dd6d06508cfddc455f1d0f9f125e6cffb6a1c
Ryan Cartier <ryan.cartier@lmnsolutions.com>
2014-05-07 10:52:57 -0400

    Refactored the rules engine

--- feed-processor/rule/ruleHandler.js
+++ feed-processor/rule/ruleHandler.js
@@ -43,55 +43,66 @@ RuleHandler.prototype.applyRule = function(rule, feedId, ruleEngineCompletionCal
 
 RuleHandler.prototype.applyDataConstraints = function (constraintSet, cb){
   if(RuleHandler.prototype.ruleInstance.type !== 'feedLevelRule'){
+
     async.eachSeries(constraintSet.entity, function(contraint, done) {
+      var promises = [];
+      //var streamObj =
+      fetcher.applyConstraints( contraint, constraintSet.fields, RuleHandler.prototype.vipFeedId, RuleHandler.prototype.ruleInstance, function(fetchedData){
 
-      var streamObj = fetcher.applyConstraints( contraint, constraintSet.fields, RuleHandler.prototype.vipFeedId, RuleHandler.prototype.ruleInstance, function(fetchedData){
+//        if(++streamObj.saveStackCount >= 10000) {
+//          streamObj.isPaused = true;
+//          streamObj.stream.pause();
+//        }
 
-        if(++streamObj.saveStackCount >= 10000) {
-          streamObj.isPaused = true;
-          streamObj.stream.pause();
-        }
+        promises.push(RuleHandler.prototype.processDataResults( fetchedData.retrieveRule.ruleDef, fetchedData.entity, fetchedData.dataResults, constraintSet));
 
-        var promise = RuleHandler.prototype.processDataResults( fetchedData.retrieveRule.ruleDef, fetchedData.entity, fetchedData.dataResults, constraintSet);
+//        if(promise) {
+//          promise.then(function() {
+//            if(--streamObj.saveStackCount === 0 && streamObj.isPaused) {
+//              streamObj.isPaused = false;
+//              streamObj.stream.resume();
+//            }
+//          });
+//        }
+//        else {
+//          if(--streamObj.saveStackCount === 0 && streamObj.isPaused) {
+//            streamObj.isPaused = false;
+//            streamObj.stream.resume();
+//          }
+//        }
 
-        if(promise) {
-          promise.then(function() {
-            if(--streamObj.saveStackCount === 0 && streamObj.isPaused) {
-              streamObj.isPaused = false;
-              streamObj.stream.resume();
-            }
-          });
-        }
-        else {
-          if(--streamObj.saveStackCount === 0 && streamObj.isPaused) {
-            streamObj.isPaused = false;
-            streamObj.stream.resume();
-          }
-        }
-      }, function() { done(); });
+      }, function() {
+        when.all(promises).then(function() {
+          done();
+        });
+      });
 
     }, function(err) { cb(); });
+
   }
   else {
     RuleHandler.prototype.processFeedLevelRule( RuleHandler.prototype.ruleInstance.ruleDef, RuleHandler.prototype.vipFeedId, constraintSet, cb );
   }
-}
+};
 
 
 RuleHandler.prototype.processDataResults = function(ruleDef, entity, result, constraintSet){
   if(constraintSet.fields.length > 0) {
-    var promises = [];
+    var retPromise = null;
+
     for(var j = 0; j < constraintSet.fields.length; j++){
       var resultItem = formatNestedResult( constraintSet.fields[j], result );
       if(resultItem != null) {
-        promises.push(RuleHandler.prototype.processRule( ruleDef, resultItem, result, entity, constraintSet.fields[j] ));
+        var promise = RuleHandler.prototype.processRule( ruleDef, resultItem, result, entity, constraintSet.fields[j] );
 
-        if(j > 0) {
-          promises[0] = when.join(promises[0], promises[j]);
-        }
+        if(j == 0)
+          retPromise = promise;
+        else
+          retPromise = when.join(retPromise, promise);
       }
     }
-   return promises[0];
+
+   return retPromise;
   }
   else {
     if(result != null) {
@@ -99,7 +110,7 @@ RuleHandler.prototype.processDataResults = function(ruleDef, entity, result, con
     }
     return null;
   }
-}
+};
 
 function formatNestedResult(field, result){
   var resultItem = null;
@@ -123,25 +134,25 @@ RuleHandler.prototype.processRule = function(ruleDef, dataItem, dataSet, entity,
 
     return null;
   });
-}
+};
 
 RuleHandler.prototype.processFeedLevelRule = function(ruleDef, feedId, constraintSet, cb){
   require(ruleDef.implementation).evaluate(feedId, constraintSet, ruleDef, function(rule){
     RuleHandler.prototype.addErrorViolations(rule.promisedErrorCount);
     cb();
   });
-}
+};
 
 RuleHandler.prototype.addErrorViolations = function addErrorViolations(promisedErrorCount){
   if(promisedErrorCount){
     violationCount += promisedErrorCount;
   }
-}
+};
 
 RuleHandler.prototype.createViolation = function createViolation(entity, dataItem, dataSet, ruleDef){
   var violation = new Violation(entity, dataSet.elementId, dataSet._id, dataSet._feed, dataSet, dataItem, ruleDef);
   violationCount++;
   return violation.getCollection().create(violation.model());
-}
+};
 
 module.exports = RuleHandler;
