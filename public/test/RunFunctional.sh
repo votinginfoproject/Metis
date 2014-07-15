9f0021734b72c564889b0f8c6da56e4983b7c985
Troy Hill <thill@snowfall.local>
2014-07-15 14:10:31 -0400

    [TH] fix functional test setup, correct README

--- public/test/RunFunctional.sh
+++ public/test/RunFunctional.sh
@@ -3,14 +3,22 @@
 # Drop the metic mongo DB
 mongo metis --eval "db.dropDatabase()"
 
+# Install counties and fips
+mongoimport -d metis -c counties < data/counties.json
+mongoimport -d metis -c statefips < data/statefips.json
+
 # Move to DB folder
-cd ../../feed-processor/
+cd feed-processor/
 
 #run processor.js
-node processor.js ../public/test/Mocked\ DB/test_feed.xml
+node processor.js ../upload/OH_test_feed.xml
 
 # Run the overview process
 node overview/overview-processor.js
 
-# Opens chrome and navigates to tests
-open /Applications/Google\ Chrome.app "http://localhost:4000/test/e2e/runner.html"
+echo "*******************"
+echo "open browser to: http://localhost:4000/test/e2e/runner.html"
+echo "*******************"
+# Run the webapp
+cd -
+node app.js
