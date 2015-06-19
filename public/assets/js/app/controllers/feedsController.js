1ca32a39de4cddd85f832da7c01a4899d10e896f
Chris Shea <cmshea@gmail.com>
2015-06-19 12:27:13 -0400

    [CS] We don't have the feed queue anymore

--- public/assets/js/app/controllers/feedsController.js
+++ public/assets/js/app/controllers/feedsController.js
@@ -13,25 +13,3 @@ function FeedsCtrl($scope, $rootScope, $feedDataPaths, $feedsService, $location,
                                key: "feeds",
                                errorMessage: "Could not retrieve Feeds."});
 }
-
-/*
- * Get the Feed Queue
- *
- */
-function FeedsCtrl_getFeedQueue($scope, $rootScope, $feedsService, servicePath, $filter, ngTableParams){
-
-  // get Feed Queue
-  $feedsService.getFeedQueue(servicePath)
-    .success(function (data) {
-
-      // set the feeds data into the Angular model
-      $scope.feedQueue = data.feedQueue;
-
-    }).error(function (data) {
-
-      $rootScope.pageHeader.error += "Could not retrieve List of Queued Feeds. ";
-
-      // so the loading spinner goes away and we are left with an empty table
-      $scope.feedQueue = {};
-    });
-}
