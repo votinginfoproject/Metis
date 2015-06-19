ae632c4ed0936aada8c5c5b118e97a262a1b0cc1
B. Wesley Reid <bwreid@gmail.com>
2015-06-19 15:17:38 -0600

    [WR] connect postgres with single contest page
    This allows all the tables to work except for the Overview table which
    will likely require the validations table to first be updated.

--- pg/services.js
+++ pg/services.js
@@ -4,6 +4,11 @@ function registerPostgresServices (app) {
   app.get('/db/results', function(req,res){ pg.getResults(req,res); });
   app.get('/db/validations', function(req,res){ pg.getValidations(req,res); });
   app.get('/db/validations/errorCount', function(req,res){ pg.getValidationsErrorCount(req,res); });
+  app.get('/db/feeds/:feedid/contests/:contestid', function(req,res){ pg.getFeedContest(req,res); });
+  app.get('/db/feeds/:feedid/contests/:contestid/ballot', function(req,res){ pg.getFeedContestBallot(req,res); });
+  app.get('/db/feeds/:feedid/contests/:contestid/electoral-district', function(req,res){ pg.getFeedContestElectoralDistrict(req,res); });
+  app.get('/db/feeds/:feedid/contests/:contestid/result', function(req,res){ pg.getFeedContestResult(req,res); });
+  app.get('/db/feeds/:feedid/contests/:contestid/ballot-line-results', function(req,res){ pg.getFeedContestBallotLineResults(req,res); });
   app.get('/db/feeds/:feedid/contests', function(req,res){ pg.getFeedContests(req,res); });
   app.get('/db/feeds/:feedid/contests/overview', pg.getFeedContestsOverview);
   app.get('/db/feeds/:feedid/overview', pg.getFeedOverview);
--- public/assets/js/app/controllers/feedContestController.js
+++ public/assets/js/app/controllers/feedContestController.js
@@ -2,89 +2,44 @@
  * Created by rcartier13 on 1/21/14.
  */
 
-function FeedContestCtrl($scope, $rootScope, $feedsService, $routeParams, $appProperties, $location, $filter, ngTableParams) {
+function FeedContestCtrl($scope, $rootScope, $feedDataPaths, $feedsService, $routeParams, $appProperties, $location, $filter, ngTableParams) {
 
   // get the vipfeed param from the route
   var feedid = $routeParams.vipfeed;
   $scope.vipfeed = feedid;
+  
+  var contestid = $routeParams.contest;
 
+  var errorPath = $feedDataPaths.getFeedValidationsErrorCountPath(feedid);
+  $feedDataPaths.getResponse({ path: errorPath,
+                               scope: $rootScope,
+                               key: "errorCount",
+                               errorMessage: "Could not retrieve Feed Error Count."});
+  $feedDataPaths.getResponse({ path: '/db/feeds/' + feedid + '/contests/' + contestid,
+                               scope:  $rootScope,
+                               key: 'feedContest',
+                               errorMessage: 'Cound not retrieve Contest ' + contestid + ' Data.'},
+                             function(result) { $rootScope.feedContest = result[0]; });
+  $feedDataPaths.getResponse({ path: '/db/feeds/' + feedid + '/contests/' + contestid + '/ballot',
+                               scope:  $rootScope,
+                               key: 'feedContestBallot',
+                               errorMessage: 'Cound not retrieve Ballot Data for Contest ' + contestid + '.'},
+                             function(result) { $rootScope.feedContestBallot = result[0]; });
+  $feedDataPaths.getResponse({ path: '/db/feeds/' + feedid + '/contests/' + contestid + '/electoral-district',
+                               scope:  $rootScope,
+                               key: 'feedContestElectoralDistrict',
+                               errorMessage: 'Cound not retrieve Electoral District Data for Contest ' + contestid + '.'},
+                             function(result) { $rootScope.feedContestElectoralDistrict = result[0]; });
+  $feedDataPaths.getResponse({ path: '/db/feeds/' + feedid + '/contests/' + contestid + '/result',
+                               scope:  $rootScope,
+                               key: 'feedContestResult',
+                               errorMessage: 'Cound not retrieve Contest Result Data for Contest ' + contestid + '.'},
+                             function(result) { $rootScope.feedContestResult = result[0]; });
+  $feedDataPaths.getResponse({ path: '/db/feeds/' + feedid + '/contests/' + contestid + '/ballot-line-results',
+                               scope:  $rootScope,
+                               key: 'feedContestBallotLineResults',
+                               errorMessage: 'Cound not retrieve Ballot Line Results Data for Contest ' + contestid + '.'},
+                             function(result) { $scope.ballotLineResultTableParams = $rootScope.createTableParams(ngTableParams, $filter, result, $appProperties.lowPagination, { id: 'asc' }); });
   // initialize page header variables
   $rootScope.setPageHeader("Contest", $rootScope.getBreadCrumbs(), "feeds", "", null);
-
-  // get general Feed data
-  $feedsService.getFeedData(feedid)
-    .success(function (data) {
-
-      // set the feeds data into the Angular model
-      $scope.feedData = data;
-      $rootScope.feedData = data;
-
-      // now call the other services to get the rest of the data
-      FeedContestCtrl_getFeedContests($scope, $rootScope, $feedsService, $rootScope.getServiceUrl($location.path()), $appProperties, $filter, ngTableParams);
-
-    }).error(function (data, $http) {
-
-      if($http===404){
-        // feed not found
-
-        $rootScope.pageHeader.alert = "Sorry, the VIP feed \"" + feedid + "\" does not exist.";
-      } else {
-        // some other error
-
-        $rootScope.pageHeader.error += "Could not retrieve Feed data. ";
-      }
-
-      // so the loading spinner goes away and we are left with an empty table
-      $scope.feedData = {};
-      $scope.feedContest = {};
-      $scope.feedContestOverview = {};
-    });
-}
-
-function FeedContestCtrl_getFeedContests($scope, $rootScope, $feedsService, servicePath, $appProperties, $filter, ngTableParams) {
-
-  $feedsService.getFeedContest(servicePath)
-    .success(function(data) {
-
-      // set the feeds data into the Angular model
-      $scope.feedContest = data;
-
-      $scope.ballotLineResultTableParams = $rootScope.createTableParams(ngTableParams, $filter, data.ballot_line_results, $appProperties.lowPagination, { id: 'asc' });
-
-      FeedContestCtrl_getBallotStyles($scope, $rootScope, $feedsService, data.ballotStyles, $appProperties, $filter, ngTableParams);
-      FeedContestCtrl_getFeedContestOverview($scope, $rootScope, $feedsService, data.overview);
-
-      // set the title
-      $rootScope.pageHeader.title = "Contest ID: " + data.id;
-
-    }).error(function(data, $http) {
-      $rootScope.pageHeader.error += "Could not retrieve Feed Contest data. ";
-
-      // so the loading spinner goes away and we are left with an empty table
-      $scope.feedContest = {};
-    });
-};
-
-function FeedContestCtrl_getBallotStyles($scope, $rootScope, $feedsService, servicePath, $appProperties, $filter, ngTableParams) {
-  $feedsService.getFeedBallot(servicePath)
-    .success(function(data) {
-      $scope.feedBallotStyle = data;
-      $scope.contestBallotStylesTableParams = $rootScope.createTableParams(ngTableParams, $filter, data, $appProperties.lowPagination, { id: 'asc' });
-
-    }).error(function(data, $http) {
-      $rootScope.pageHeader.error += "Could not retrieve Feed Ballot Style data";
-
-      $scope.ballotStyle = {};
-    })
-}
-
-function FeedContestCtrl_getFeedContestOverview($scope, $rootScope, $feedsService, servicePath) {
-  $feedsService.getFeedContest(servicePath)
-    .success(function(data) {
-      $scope.feedContestOverview = data;
-    }).error(function(data, $http) {
-      $rootScope.pageHeader.error += "Could not retrieve Feed Contest Overview data. "
-      // so the loading spinner goes away and we are left with an empty table
-      $scope.feedContestOverview = {};
-    })
 }
