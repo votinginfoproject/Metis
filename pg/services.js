2b3ed2ed22febedf258d22db209592524b62dad3
B. Wesley Reid <bwreid@gmail.com>
2015-06-26 10:18:19 -0600

    [WR] connect single ballot page with pg

--- pg/services.js
+++ pg/services.js
@@ -5,6 +5,10 @@ function registerPostgresServices (app) {
   app.get('/db/feeds/:feedid/contests', pg.getFeedContests);
   app.get('/db/feeds/:feedid/contests/:contestid', pg.getFeedContest);
   app.get('/db/feeds/:feedid/contests/:contestid/ballot', pg.getFeedContestBallot);
+  app.get('/db/feeds/:feedid/contests/:contestid/ballot/referendum', pg.getFeedContestBallotReferendum);
+  app.get('/db/feeds/:feedid/contests/:contestid/ballot/candidates', pg.getFeedContestBallotCandidates);
+  app.get('/db/feeds/:feedid/contests/:contestid/ballot/custom-ballot', pg.getFeedContestBallotCustomBallot);
+  app.get('/db/feeds/:feedid/contests/:contestid/ballot/custom-ballot-responses', pg.getFeedContestBallotCustomBallotResponses);
   app.get('/db/feeds/:feedid/contests/:contestid/ballot-line-results', pg.getFeedContestBallotLineResults);
   app.get('/db/feeds/:feedid/contests/:contestid/electoral-district', pg.getFeedContestElectoralDistrict);
   app.get('/db/feeds/:feedid/contests/:contestid/result', pg.getFeedContestResult);
