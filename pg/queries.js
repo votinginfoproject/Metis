e8ce4d89f14b56a75501c80c531d3dadf13ee06e
B. Wesley Reid <bwreid@gmail.com>
2015-06-29 18:00:07 -0600

    [WR] change move queries in gets.js to queries.js

--- pg/queries.js
+++ pg/queries.js
@@ -1,4 +1,130 @@
 module.exports = {
+  feeds: "SELECT r.public_id, date(r.start_time) AS date, \
+                 CASE WHEN r.end_time IS NOT NULL THEN r.end_time - r.start_time END AS duration, \
+                 r.complete, s.name AS state, e.election_type, e.date \
+          FROM results r \
+          LEFT JOIN states s ON s.results_id = r.id \
+          LEFT JOIN elections e ON e.results_id = r.id \
+          ORDER BY r.start_time DESC;",
+  results: "SELECT * FROM results WHERE public_id=$1",
+  contest: "SELECT c.*, \
+                   (SELECT COUNT(v.*) \
+                    FROM validations v \
+                    WHERE r.id = v.results_id AND v.scope = 'contests') AS error_count \
+            FROM contests c \
+            INNER JOIN results r ON r.id = c.results_id \
+            WHERE r.public_id=$1 AND c.id=$2;",
+  contestBallot: "SELECT c.ballot_id, \
+                         (SELECT COUNT(b.referendum_id) \
+                          FROM ballots b \
+                          WHERE b.id=c.ballot_id AND b.results_id = c.results_id) AS referendum_count, \
+                         (SELECT COUNT(*) \
+                          FROM ballot_candidates bc \
+                          WHERE bc.results_id=c.results_id AND ballot_id=c.ballot_id) AS candidate_count \
+                  FROM contests c \
+                  INNER JOIN results r ON r.id=c.results_id \
+                  WHERE r.public_id=$1 AND c.id=$2;",
+  contestBallotReferendum: "SELECT ref.* \
+                            FROM contests c \
+                            LEFT JOIN ballots b ON b.id = c.ballot_id AND b.results_id = c.results_id \
+                            INNER JOIN referendums ref ON ref.id = b.referendum_id AND ref.results_id = c.results_id \
+                            LEFT JOIN results r ON r.id=c.results_id \
+                            WHERE r.public_id=$1 AND c.id=$2;",
+  contestBallotCustomBallot: "SELECT cb.* \
+                              FROM contests c \
+                              LEFT JOIN ballots b ON b.custom_ballot_id = c.ballot_id AND b.results_id = c.results_id \
+                              LEFT JOIN custom_ballots cb ON cb.id = b.custom_ballot_id AND cb.results_id = c.results_id \
+                              LEFT JOIN results r ON r.id=c.results_id \
+                              WHERE r.public_id=$1 AND c.id=$2;",
+  contestBallotCustomBallotResponses: "SELECT br.*, \
+                                       (SELECT COUNT(v.*) \
+                                        FROM validations v \
+                                        WHERE v.results_id=c.results_id AND scope = 'ballot_responses' AND identifier = br.id) AS error_count \
+                                       FROM contests c \
+                                       LEFT JOIN ballots b ON b.custom_ballot_id = c.ballot_id AND b.results_id = c.results_id \
+                                       LEFT JOIN custom_ballots cb ON cb.id = b.custom_ballot_id AND cb.results_id = c.results_id \
+                                       LEFT JOIN custom_ballot_ballot_responses cbbc ON cbbc.custom_ballot_id = cb.id AND cbbc.results_id = c.results_id \
+                                       LEFT JOIN ballot_responses br ON br.id = cbbc.ballot_response_id AND br.results_id = c.results_id \
+                                       LEFT JOIN results r ON r.id=c.results_id \
+                                       WHERE r.public_id=$1 AND c.id=$2;",
+  contestElectoralDistrict: "SELECT c.electoral_district_id, e.name, \
+                                    (SELECT COUNT(*) \
+                                     FROM precinct_electoral_districts ped \
+                                     WHERE ped.electoral_district_id = c.electoral_district_id) AS precinct_count, \
+                                    (SELECT COUNT(*) \
+                                     FROM precinct_split_electoral_districts psed \
+                                     WHERE psed.electoral_district_id = c.electoral_district_id) AS precinct_split_count \
+                             FROM contests c \
+                             INNER JOIN electoral_districts e ON c.electoral_district_id = e.id \
+                             INNER JOIN results r ON r.id = c.results_id \
+                             WHERE r.public_id=$1 AND c.id=$2;",
+  contestResult: "SELECT cr.id, cr.total_votes, cr.total_valid_votes, cr.overvotes, cr.blank_votes, cr.certification \
+                  FROM contests c \
+                  INNER JOIN contest_results cr ON cr.contest_id = c.id \
+                  INNER JOIN results r ON r.id = c.results_id \
+                  WHERE r.public_id=$1 AND c.id=$2;",
+  contestBallotLineResults: "SELECT blr.id, blr.votes, blr.certification \
+                             FROM contests c \
+                             INNER JOIN ballot_line_results blr ON blr.contest_id = c.id \
+                             INNER JOIN results r ON r.id = c.results_id \
+                             WHERE r.public_id=$1 AND c.id=$2;",
+  contests: "SELECT c.* \
+             FROM contests c \
+             INNER JOIN results r ON r.id = c.results_id \
+             WHERE r.public_id=$1",
+  localities: "SELECT l.id, l.name, COUNT(p.*) AS precincts \
+               FROM localities l \
+               INNER JOIN precincts p ON p.locality_id = l.id AND p.results_id = l.results_id \
+               INNER JOIN results r ON l.results_id = r.id \
+               WHERE r.public_id = $1 GROUP BY l.id, l.name, l.results_id \
+               ORDER BY l.id;",
+  state: "SELECT s.id, s.name, \
+                 (SELECT COUNT(l.*) \
+                  FROM localities l \
+                  WHERE l.results_id = s.results_id) AS locality_count, \
+                 (SELECT COUNT(v.*) \
+                  FROM validations v \
+                  WHERE v.results_id = s.results_id AND v.scope = 'states') AS error_count \
+          FROM states s \
+          INNER JOIN results r ON s.results_id = r.id \
+          WHERE r.public_id = $1 GROUP BY s.id, s.name, s.results_id ORDER BY s.id;",
+  electionAdministrations: "SELECT e.id, e.name, \
+                                   CONCAT(e.physical_address_city, ', ', \
+                                          e.physical_address_state, ', ', \
+                                          e.physical_address_zip) AS address \
+                            FROM election_administrations e \
+                            INNER JOIN results r ON e.results_id = r.id \
+                            WHERE r.public_id=$1;",
+  election: "SELECT e.*, \
+                    (SELECT COUNT(v.*) \
+                     FROM validations v \
+                     WHERE v.results_id = e.results_id AND scope='elections') AS error_count \
+             FROM elections e \
+             INNER JOIN results r ON r.id = e.results_id \
+             WHERE r.public_id=$1",
+  referendum: "SELECT ref.*, \
+                      (SELECT COUNT(v.*) \
+                       FROM validations v \
+                       WHERE v.results_id = ref.results_id AND scope='referendums' AND identifier = ref.id) AS error_count \
+               FROM referendums ref \
+               INNER JOIN results r ON r.id = ref.results_id \
+               WHERE r.public_id=$1 AND ref.id = $2;",
+  referendumBallotResponses: "SELECT br.*, \
+                                     (SELECT COUNT(v.*) \
+                                      FROM validations v \
+                                      WHERE v.results_id = br.results_id AND scope='ballot_responses' AND identifier = br.id) AS error_count \
+                              FROM referendums ref \
+                              INNER JOIN referendum_ballot_responses rbr ON ref.id = rbr.referendum_id AND rbr.results_id = ref.results_id \
+                              INNER JOIN ballot_responses br ON br.id = rbr.ballot_response_id AND br.results_id = ref.results_id \
+                              INNER JOIN results r ON r.id = ref.results_id \
+                              WHERE r.public_id=$1 AND ref.id = $2;",
+  source: "SELECT s.*, \
+                  (SELECT COUNT(v.*) \
+                   FROM validations v \
+                   WHERE v.results_id = s.results_id AND scope='sources') AS error_count \
+           FROM sources s \
+           INNER JOIN results r ON r.id = s.results_id \
+           WHERE r.public_id=$1",
   contestBallotCandidate: "SELECT can.*, (SELECT COUNT(v.*) \
                                           FROM validations v \
                                           WHERE v.scope='candidates' AND v.identifier = can.id AND v.results_id = can.results_id) \
