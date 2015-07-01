65edbad8dfa7867ef1b69fe98ace5818bea16cdb
B. Wesley Reid <bwreid@gmail.com>
2015-07-01 15:01:35 -0600

    [WR] reorder services.js

--- pg/services.js
+++ pg/services.js
@@ -2,6 +2,16 @@ var pg = require("./gets.js");
 
 function registerPostgresServices (app) {
   app.get('/db/feeds', pg.getFeeds);
+  app.get('/db/feeds/:feedid/election', pg.getFeedElection);
+  app.get('/db/feeds/:feedid/election/state', pg.getFeedState);
+  app.get('/db/feeds/:feedid/election/state/election-administrations', pg.getFeedElectionAdministrations);
+  app.get('/db/feeds/:feedid/localities', pg.getFeedLocalities);
+  app.get('/db/feeds/:feedid/overview', pg.getFeedOverview);
+  app.get('/db/feeds/:feedid/results', pg.getResults);
+  app.get('/db/feeds/:feedid/source', pg.getFeedSource);
+  app.get('/db/feeds/:feedid/validations/errorCount', pg.getValidationsErrorCount);
+
+  // Contest-related routes
   app.get('/db/feeds/:feedid/contests', pg.getFeedContests);
   app.get('/db/feeds/:feedid/contests/:contestid', pg.getFeedContest);
   app.get('/db/feeds/:feedid/contests/:contestid/ballot', pg.getFeedContestBallot);
@@ -14,9 +24,8 @@ function registerPostgresServices (app) {
   app.get('/db/feeds/:feedid/contests/:contestid/electoral-district', pg.getFeedContestElectoralDistrict);
   app.get('/db/feeds/:feedid/contests/:contestid/result', pg.getFeedContestResult);
   app.get('/db/feeds/:feedid/contests/overview', pg.getFeedContestsOverview);
-  app.get('/db/feeds/:feedid/election', pg.getFeedElection);
-  app.get('/db/feeds/:feedid/election/state', pg.getFeedState);
-  app.get('/db/feeds/:feedid/election/state/election-administrations', pg.getFeedElectionAdministrations);
+  
+  // Locality-related routes
   app.get('/db/feeds/:feedid/election/state/localities/:localityid', pg.getFeedLocality);
   app.get('/db/feeds/:feedid/election/state/localities/:localityid/early-vote-sites', pg.getFeedLocalityEarlyVoteSites);
   app.get('/db/feeds/:feedid/election/state/localities/:localityid/election-administration', pg.getFeedLocalityElectionAdministration);
@@ -27,19 +36,18 @@ function registerPostgresServices (app) {
   app.get('/db/feeds/:feedid/election/state/localities/:localityid/overview/precincts', pg.getFeedLocalityOverviewPrecincts);
   app.get('/db/feeds/:feedid/election/state/localities/:localityid/overview/precinct-splits', pg.getFeedLocalityOverviewPrecinctSplits);
   app.get('/db/feeds/:feedid/election/state/localities/:localityid/overview/street-segments', pg.getFeedLocalityOverviewStreetSegments);
+  
+  // Precinct-related routes
   app.get('/db/feeds/:feedid/precinct/:precinctid', pg.getFeedPrecinct);
   app.get('/db/feeds/:feedid/precinct/:precinctid/early-vote-sites', pg.getFeedPrecinctEarlyVoteSites);
   app.get('/db/feeds/:feedid/precinct/:precinctid/electoral-districts', pg.getFeedPrecinctElectoralDistricts);
   app.get('/db/feeds/:feedid/precinct/:precinctid/polling-locations', pg.getFeedPrecinctPollingLocations);
   app.get('/db/feeds/:feedid/precinct/:precinctid/precinct-splits', pg.getFeedPrecinctPrecinctSplits);
   app.get('/db/feeds/:feedid/precinct/:precinctid/street-segments', pg.getFeedPrecinctStreetSegments);
-  app.get('/db/feeds/:feedid/localities', pg.getFeedLocalities);
-  app.get('/db/feeds/:feedid/overview', pg.getFeedOverview);
+  
+  // Referendum-related routes
   app.get('/db/feeds/:feedid/referendum/:referendumid', pg.getFeedReferendum);
   app.get('/db/feeds/:feedid/referendum/:referendumid/ballot-responses', pg.getFeedReferendumBallotResponses);
-  app.get('/db/feeds/:feedid/results', pg.getResults);
-  app.get('/db/feeds/:feedid/source', pg.getFeedSource);
-  app.get('/db/feeds/:feedid/validations/errorCount', pg.getValidationsErrorCount);
 }
 
 exports.registerPostgresServices = registerPostgresServices;
