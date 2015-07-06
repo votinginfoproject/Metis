31b685e190350bb59153c78f89ee367ccd34357f
B. Wesley Reid <bwreid@gmail.com>
2015-07-06 17:21:16 -0400

    [WR] cleanup contests page

--- public/assets/js/app/controllers/feedContestsController.js
+++ public/assets/js/app/controllers/feedContestsController.js
@@ -4,30 +4,31 @@
  */
 
 function FeedContestsCtrl($scope, $rootScope, $feedDataPaths, $feedsService, $routeParams, $appProperties, $location, $filter, ngTableParams) {
-
-  // get the vipfeed param from the route
-  var feedid = $routeParams.vipfeed;
-  $scope.vipfeed = feedid;
-
-  // initialize page header variables
-  $rootScope.setPageHeader("Contests", $rootScope.getBreadCrumbs(), "feeds", "", null);
+  var feedid = $scope.vipfeed = $routeParams.vipfeed;
 
   var errorPath = $feedDataPaths.getFeedValidationsErrorCountPath(feedid);
   $feedDataPaths.getResponse({ path: errorPath,
                                scope: $rootScope,
                                key: "errorCount",
                                errorMessage: "Could not retrieve Feed Error Count."});
+  
   $feedDataPaths.getResponse({ path: '/db/feeds/' + feedid + '/contests',
                                scope: $rootScope,
                                key: "feedContests",
                                errorMessage: "Could not retrieve Feed Contests data. "},
                              function(data) {
-                               $scope.contestsTableParams = $rootScope.createTableParams(ngTableParams, $filter, data, $appProperties.highPagination, { id: 'asc' });
+                               $scope.contestsTable = $rootScope.createTableParams(ngTableParams, $filter, data, $appProperties.highPagination, { id: 'asc' });
                                $rootScope.pageHeader.title = data.length + " Contests";
                              });
+  
   $feedDataPaths.getResponse({ path: '/db/feeds/' + feedid + '/overview',
                                scope: $rootScope,
                                key: "feedContestsOverview",
                                errorMessage: "Could not retrieve Feed Contests Overview data."},
-                             function(result) { $rootScope.feedContestsOverview = result[0]["contests"]; });
+                             function(result) {
+                              $rootScope.feedContestsOverview = result[0]["contests"];
+                              $scope.contestsOverviewTable = $rootScope.createTableParams(ngTableParams, $filter, result[0]["contests"], $appProperties.highPagination, { id: 'asc' });
+                            });
+
+  $rootScope.setPageHeader("Contests", $rootScope.getBreadCrumbs(), "feeds", "", null);
 }
