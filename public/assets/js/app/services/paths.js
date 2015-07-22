0423fd5ce43921dadc5529c63dfb66f3ca118b4c
Wes Reid <bwreid@gmail.com>
2015-07-22 19:21:04 +0200

    [WR] remove unused paths in $feedDataPaths

--- public/assets/js/app/services/paths.js
+++ public/assets/js/app/services/paths.js
@@ -2,10 +2,7 @@
 
 vipApp.factory('$feedDataPaths', function ($http) {
   return {
-    getFeedResultsPath: function (feedid) { return '/db/feeds/' + feedid + '/results' },
     getFeedValidationsErrorCountPath: function (feedid) { return '/db/feeds/' + feedid + '/validations/errorCount' },
-    getFeedContestsPath: function (feedid) { return "/db/feeds/" + feedid + "/contests" },
-    getContestsOverviewTablePath: function(feedid) { return "/db/feeds/" + feedid + "/contests/overview" },
     getResponse: function(options, callback) {
       $http.get(options['path']).
         success(function (results, status) {
