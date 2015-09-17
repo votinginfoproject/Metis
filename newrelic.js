81636f24d3e6880b915a7a768f4b475529c7108b
Wes Reid <bwreid@gmail.com>
2015-09-17 13:24:37 -0600

    [WR] include new relic on the dashboard front-end
    Takes environment variables to configure newrelic.js

--- newrelic.js
+++ newrelic.js
@@ -4,15 +4,16 @@
  * See lib/config.defaults.js in the agent distribution for a more complete
  * description of configuration variables and their potential values.
  */
+
 exports.config = {
   /**
    * Array of application names.
    */
-  app_name : ['Metis'],
+  app_name : ['Data Dashboard (' + process.env.NEW_RELIC_ENVIRONMENT + ')'],
   /**
    * Your New Relic license key.
    */
-  license_key : 'license key goes here',
+  license_key : process.env.NEW_RELIC_LICENSE_KEY,
   logging : {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
