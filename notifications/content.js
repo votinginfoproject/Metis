1367a5f04a080f0cb19d5df1b5917eaa12ff4f43
Wes Reid <bwreid@gmail.com>
2015-10-01 15:46:08 -0600

    [WR] use env var to set link in email

--- notifications/content.js
+++ notifications/content.js
@@ -1,9 +1,11 @@
+var baseUrl = process.env.BASE_URI || "localdocker:4000";
+
 module.exports = {
   processedFeed: function(message, recipient, group) {
     return "<p>" + recipient.givenName + ",</p>" +
            "<p>The data you provided for " + group.description + "'s election is available for you to review on the VIP Data Dashboard.</p>" +
            "<p>Please click the link below to review your feed.</p>" +
-           "<p><a href='http://dashboard.votinginfoproject.org/#/feeds/" + message[":public-id"] + "'>Go to the Data Dashboard</a></p>" + 
+           "<p><a href='http://" + baseUrl + "/#/feeds/" + message[":public-id"] + "'>Go to the Data Dashboard</a></p>" + 
            "<p>If you have any questions, please contact <a href='mailto:vip@democracy.works'>vip@democracy.works</a>.</p>" +
            "<p>Thank you!</p>";
   },
