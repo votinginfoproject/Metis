80efa21c4205c27ee956ce2bdde0b847d843fc05
Wes Reid <bwreid@gmail.com>
2015-10-08 15:41:43 -0600

    [WR] send email when approval buton is clicked

--- public/assets/js/app/controllers/asideController.js
+++ public/assets/js/app/controllers/asideController.js
@@ -1,5 +1,25 @@
-function AsideCtrl($scope, $rootScope, $feedDataPaths, $routeParams) {
+function AsideCtrl($scope, $rootScope, $http, $feedDataPaths, $routeParams) {
   var feedid = $routeParams.vipfeed;
+
+  function sendEmail(election) {
+    $http.post('/notifications/approve-feed', { 
+      ":public-id": feedid,
+      adminEmail: true,
+      election: election,
+      user: $scope.$parent.user
+    }).then(function() { console.log('Notification has been sent.') },
+            function() { console.log('Notification encountered a problem.') });
+  }
+
+  $rootScope.notifyApproveFeed = function() {
+    $feedDataPaths.getResponse({ path: '/db/feeds/' + feedid + '/election',
+                                 scope: $rootScope,
+                                 key: "election",
+                                 errorMessage: "Could not send the approve feed email."},
+                               function(result) {
+                                sendEmail(result[0]);
+                              });
+  };
 
   $feedDataPaths.getResponse({ path: '/db/feeds/' + feedid + '/error-total-count',
                                scope: $rootScope,
