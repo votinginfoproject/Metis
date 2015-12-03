dae3aee4b521d57a627f2de7bf11ed0726941ee3
Chris Shea <cmshea@gmail.com>
2015-12-03 12:25:04 -0500

    [CS] Add time to "Loaded on" column of feeds list

--- pg/queries.js
+++ pg/queries.js
@@ -26,7 +26,7 @@ var buildErrorQuery = function(joins, wheres) {
 
 module.exports = {
   feeds: "SELECT DISTINCT ON (r.id) \
-                 r.public_id, date(r.start_time) AS start_time, date(r.end_time) AS end_time, \
+                 r.public_id, r.start_time, date(r.end_time) AS end_time, \
                  CASE WHEN r.end_time IS NOT NULL THEN r.end_time - r.start_time END AS duration, \
                  r.complete, s.name AS state, e.election_type, date(e.date) AS election_date \
           FROM results r \
