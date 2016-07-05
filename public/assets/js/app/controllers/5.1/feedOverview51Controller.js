ee491b819f7f53bef44bd5849c32a89fe5971907
Sean Duckett <sduckett@gmail.com>
2016-07-05 10:49:42 -0600

    5.1 overview starts with a title
    The title is built from 'election_date', 'election_type', and 'state'

--- public/assets/js/app/controllers/5.1/feedOverview51Controller.js
+++ public/assets/js/app/controllers/5.1/feedOverview51Controller.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function FeedOverview51Ctrl($scope, $routeParams) {
-  var publicId = $routeParams.vipfeed;
-  $scope.publicId = publicId;
+  var publicId = $scope.publicId = $routeParams.vipfeed;
+
+  $scope.pageTitle = formatVipFeedID(publicId);
   $scope.errorReport = "/db/feeds/" + publicId + "/xml/errors/report";
 }
