d6748b65deed991140618183ede123a3b9490ba7
Sean Duckett <sduckett@gmail.com>
2016-07-07 09:26:55 -0600

    Improve path helpers

--- public/assets/js/app/controllers/5.1/v5AsideController.js
+++ public/assets/js/app/controllers/5.1/v5AsideController.js
@@ -1,3 +1,6 @@
-function v5AsideController($routeParams) {
-  var feedid = $routeParams.vipfeed;
+function v5AsideController($scope, $rootScope, $routeParams) {
+  var publicId = $scope.publicId = $routeParams.vipfeed;
+  $rootScope.feedURL = function(path) {
+    return "#/5.1/feeds/" + $scope.publicId + path;
+  };
 }
