0ff47f952c7488b5aae88040074b1caa18da9f70
Sean Duckett <sduckett@gmail.com>
2016-07-19 16:04:30 -0600

    Add Voting Locations errors to 5.1 feed overview

--- public/assets/js/app/controllers/5.1/feedOverview51Controller.js
+++ public/assets/js/app/controllers/5.1/feedOverview51Controller.js
@@ -1,19 +1,35 @@
 'use strict';
 
-function FeedOverview51Ctrl($scope, $rootScope, $feedDataPaths, $routeParams) {
+function FeedOverview51Ctrl($scope, $rootScope, $feedDataPaths, $routeParams,
+                            $location, $appProperties, $filter, ngTableParams) {
   var publicId = $rootScope.publicId = $routeParams.vipfeed;
   $rootScope.errorReport = "/db/feeds/" + publicId + "/xml/errors/report";
 
-  $feedDataPaths.getResponse({path: '/db/feeds/' + publicId + '/xml/overview',
-                              scope:  $rootScope,
-                              key: 'overviewData',
-                              errorMessage: 'Cound not retrieve Feed Overview Data.'},
-                             function(result) {
-                               var row = result[0];
-                               $scope.pageTitle = row.election_date + " " +
-                                                  row.election_type + " " +
-                                                  row.state_name;
-                               $rootScope.pageTitle = $scope.pageTitle;
-                               $rootScope.setPageHeader($scope.pageTitle, [], 'feeds', null, null);
-                             });
+  $feedDataPaths
+    .getResponse({path: '/db/feeds/' + publicId + '/xml/overview',
+                  scope:  $rootScope,
+                  key: 'overviewData',
+                  errorMessage: 'Cound not retrieve Feed Overview Data.'},
+                 function(result) {
+                   var row = $rootScope.overviewData = result.shift();
+                   $scope.pageTitle = row.election_date + " " +
+                     row.election_type + " " +
+                     row.state_name;
+                   $rootScope.pageTitle = $scope.pageTitle;
+                   $rootScope.setPageHeader($scope.pageTitle, [], 'feeds', null, null);
+                 });
+
+  $feedDataPaths
+    .getResponse({path: 'db/feeds/' + publicId + '/xml/localityOverview',
+                  scope: $rootScope,
+                  key: 'pollingLocations',
+                  errorMessage: 'Could not get locality data'},
+                 function(result) {
+                   var row = $rootScope.pollingLocationsData = result.shift();
+                   $scope.pollingLocationsTable =
+                     $rootScope.createTableParams(ngTableParams, $filter,
+                                                  row.pollingLocations,
+                                                  $appProperties.lowPagination,
+                                                  { element_type: 'asc' });
+                 });
 }
