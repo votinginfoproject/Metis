e3035428f930fe75812612d901d34effd6228dd1
Katrina Hall <katrina.m.hall@gmail.com>
2017-04-25 14:13:14 -0400

    currently works to upload files
    additionally requires an AWSAccessKeyId, policy, and signature that I'm not committing

--- public/assets/js/app/controllers/feedsController.js
+++ public/assets/js/app/controllers/feedsController.js
@@ -3,8 +3,23 @@
  * Feeds Controller
  *
  */
-function FeedsCtrl($scope, $rootScope, $feedDataPaths, $feedsService, $location, $filter, ngTableParams, $interval, $timeout, $route) {
-
+function FeedsCtrl($scope, $rootScope, $feedDataPaths, $feedsService, $location, $filter, ngTableParams, $interval, $timeout, $route, Upload) {
+  $scope.upload = function(file) {
+    Upload.upload({
+    url: 'https://khall-test.s3.amazonaws.com/', //S3 upload url including bucket name
+    method: 'POST',
+    data: {
+        key: file.name, // the key to store the file on S3, could be file name or customized
+        AWSAccessKeyId: "",
+        acl: 'private', // sets the access to the uploaded file in the bucket: private, public-read, ...
+        policy: '',
+        signature: '', // base64-encoded signature based on policy string (see article below)
+        "Content-Type": file.type != '' ? file.type : 'application/octet-stream', // content type of the file (NotEmpty)
+        // filename: file.name, // this is needed for Flash polyfill IE8-9
+        file: file
+        }
+  });
+};
   // initialize page header variables
   $rootScope.setPageHeader("Feeds", $rootScope.getBreadCrumbs(), "feeds", null);
 
