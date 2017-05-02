c59e769187952bd9ebd7ced294a47e6f1a01a958
Troy Hill <troy@democracy.works>
2017-05-02 12:28:36 +0200

    add new testing page and vit embed

---
+++ public/assets/js/app/controllers/testing/vitController.js
@@ -0,0 +1,10 @@
+'use strict';
+/*
+ * VIT Staging Controller
+ *
+ */
+function VitCtrl($scope, $rootScope) {
+  var breadcrumbs = null;
+  // initialize page header variables
+  $rootScope.setPageHeader("VIT Staging Tool", breadcrumbs, "testing", "", null);
+}
