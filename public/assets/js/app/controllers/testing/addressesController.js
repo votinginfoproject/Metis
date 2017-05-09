0c2bc8871ceeac2440b1de6cb1eeb9517ed9685b
Troy Hill <troy@democracy.works>
2017-05-09 15:59:19 +0200

    add page for address batch checking

---
+++ public/assets/js/app/controllers/testing/addressesController.js
@@ -0,0 +1,11 @@
+'use strict';
+/*
+ * Batch Addresses Controller
+ *
+ */
+function AddressesCtrl($scope, $rootScope) {
+  var breadcrumbs = null;
+  // initialize page header variables
+  $rootScope.setPageHeader("Batch Check Test Addresses", breadcrumbs, "testing", "", null);
+
+};
