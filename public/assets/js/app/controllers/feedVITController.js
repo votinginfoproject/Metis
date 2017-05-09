3422deda1694fa7daf8efc22a3699057354737b1
Troy Hill <troy@democracy.works>
2017-05-09 11:12:15 +0200

    remove last bit of previous feed nav based VIT

--- public/assets/js/app/controllers/feedVITController.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-/*
- * VIT Staging Controller
- *
- */
-function FeedVITCtrl($scope, $rootScope, $homeService, $routeParams) {
-  // initialize page header variables
-  $rootScope.setPageHeader("Voter Information Tool Staging", $rootScope.getBreadCrumbs(), "feeds", "", null);
-}
