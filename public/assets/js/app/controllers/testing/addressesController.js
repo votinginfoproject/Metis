7c668577389237330d426c300b1be49609d8b5ce
Katrina Hall <katrina.m.hall@gmail.com>
2017-05-23 16:19:46 -0400

    button on batch address page can upload to s3 bucket

--- public/assets/js/app/controllers/testing/addressesController.js
+++ public/assets/js/app/controllers/testing/addressesController.js
@@ -3,9 +3,24 @@
  * Batch Addresses Controller
  *
  */
-function AddressesCtrl($scope, $rootScope) {
+function AddressesCtrl($scope, $rootScope, Upload) {
   var breadcrumbs = null;
   // initialize page header variables
   $rootScope.setPageHeader("Batch Check Test Addresses", breadcrumbs, "testing", "", null);
+
+  $scope.upload = function (file) {
+        Upload.upload({
+            url: '/testing/upload',
+            data: {file: file, 'username': $scope.username}
+        }).then(function (resp) {
+            console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+        }, function (resp) {
+            console.log('Error status: ' + resp.status);
+        }, function (evt) {
+            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+            console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+        });
+    };
+
 
 };
