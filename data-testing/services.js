dc1ed8c2d1a191a02f4eaf93392821c932634a53
Katrina Hall <katrina.m.hall@gmail.com>
2017-06-06 17:21:52 -0400

    added metis endpoint to look for latest results file to populate on address testing page

--- data-testing/services.js
+++ data-testing/services.js
@@ -2,6 +2,7 @@ var aws = require("./aws.js");
 
 function registerDataVerificationServices (app) {
   app.post('/testing/upload', aws.uploadAddressFile);
+  app.get('/testing/latest-results-file', aws.getLatestResultsFile);
 }
 
 exports.registerDataVerificationServices = registerDataVerificationServices;
