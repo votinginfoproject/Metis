ee8a0c4a1d09d4ee087bfe0521ecf6e885159084
Troy Hill <troy@democracy.works>
2017-07-20 09:49:51 -0600

    create auth service

---
+++ public/assets/js/app/services/authService.js
@@ -0,0 +1,56 @@
+'use strict';
+
+vipApp.factory('$authService', function ($location, $timeout, angularAuth0) {
+  
+  function login() {
+    angularAuth0.authorize();
+  }
+
+  function handleAuthentication() {
+    angularAuth0.parseHash(function(err, authResult) {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        console.log("handleAuthentication: success");
+        setSession(authResult);
+        $location.url('/feeds');
+      } else if (err) {
+        $timeout(function() {
+          $location.url('/');
+        });
+        console.log(err);
+      }
+    });
+  }
+
+  function setSession(authResult) {
+    // Set the time that the access token will expire at
+    let expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+    localStorage.setItem('auth0_access_token', authResult.accessToken);
+    localStorage.setItem('auth0_id_token', authResult.idToken);
+    localStorage.setItem('auth0_expires_at', expiresAt);
+  }
+
+  function logout() {
+    // Remove tokens and expiry time from localStorage
+    localStorage.removeItem('auth0_access_token');
+    localStorage.removeItem('auth0_id_token');
+    localStorage.removeItem('auth0_expires_at');
+  }
+
+  function isAuthenticated() {
+    // Check whether the current time is past the 
+    // access token's expiry time
+    let expiresAt = JSON.parse(localStorage.getItem('auth0_expires_at'));
+    let authenticated = new Date().getTime() < expiresAt;
+    console.log("isAuthenticated: " + authenticated);
+    return authenticated;
+  }
+  
+
+  return {
+    login: login,
+    handleAuthentication: handleAuthentication,
+    setSession: setSession,
+    logout: logout,
+    isAuthenticated: isAuthenticated
+  }
+});
