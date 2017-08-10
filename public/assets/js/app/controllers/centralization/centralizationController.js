2de229bcf7b2beabe72c5472e69b25c5bdc842de
Katrina Hall <katrina.m.hall@gmail.com>
2017-08-10 15:29:27 -0400

    WIP adding new data to form and submitting the form instead of just a file

--- public/assets/js/app/controllers/centralization/centralizationController.js
+++ public/assets/js/app/controllers/centralization/centralizationController.js
@@ -6,25 +6,28 @@
 function CentralizationCtrl($scope, $rootScope, Upload, $configService, $route) {
   var breadcrumbs = null;
   // initialize page header variables
-  $scope.setPageHeader("VIP County Data Centralization Upload", breadcrumbs, "testing", "", null);
+  $scope.setPageHeader("VIP County Data Centralization Upload", breadcrumbs, "centralization", "", null);
 
-  $scope.pageHeader.title = 'VIP County Data Centralization Upload';
-  $scope.upload = function (file) {
-        Upload.upload({
-            url: '/testing/upload',
-            data: {file: file,
-                   'username': $scope.username,
-                   'fipsCode': $rootScope.user.fipsCodes[0]}
-        }).then(function (resp) {
-            console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
-            $rootScope.showUploaded = true;
-            $route.reload();
-        }, function (resp) {
-            console.log('Error status: ' + resp.status);
-        }, function (evt) {
-            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-            console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
-        });
+  $scope.submit = function() {
+    // check that we have a date and a file
+    $scope.upload($scope.form);
+  };
+  $scope.upload = function (form) {
+        console.log(form);
+        // Upload.upload({
+        //     url: '/testing/upload',
+        //     data: {file: file,
+        //            'fipsCode': $rootScope.user.fipsCodes[0]}
+        // }).then(function (resp) {
+        //     console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+        //     $rootScope.showUploaded = true;
+        //     $route.reload();
+        // }, function (resp) {
+        //     console.log('Error status: ' + resp.status);
+        // }, function (evt) {
+        //     var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+        //     console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+        // });
     };
 
     $configService.getResponse({path: '/testing/latest-results-file', config: {params: {'fipsCode': $rootScope.user.fipsCodes[0]}}},
