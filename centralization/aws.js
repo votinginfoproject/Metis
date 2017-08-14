b33e1c5aba1856191649be1393cf69e2a7df2db5
Troy Hill <troy@democracy.works>
2017-08-14 10:06:39 -0600

    first pass implementation of file uploading

--- centralization/aws.js
+++ centralization/aws.js
@@ -5,7 +5,7 @@ var config = require('../config');
 var logger = (require('../logging/vip-winston')).Logger;
 
 AWS.config.update({ accessKeyId: config.aws.accessKey, secretAccessKey: config.aws.secretKey });
-var batchAddressBucket = config.batt.batchAddressBucket;
+var centralizationBucket = config.dataCentralization.centralizationBucket;
 
 module.exports = {
   uploadFile: function(req, res){
@@ -13,35 +13,35 @@ module.exports = {
     form.parse(req, function(err, fields, files) {
       console.log(fields.date);
       console.log(files.file.path);
-
-    //   var fipsCode = fields["fipsCode"];
-    //   res.writeHead(200, {'content-type': 'text/plain'});
-    //   res.write('received upload:\n\n');
-    //   res.end(JSON.stringify(files));
-    //   var fileStream = fs.createReadStream(files.file.path);
-    //   fileStream.on('error', function (err) {
-    //     if (err) { throw err; }
-    //   });
-    //   fileStream.on('open', function () {
-    //     var s3 = new AWS.S3();
-    //     if (fipsCode === undefined) {
-    //       fipsCode = "undefined"
-    //     };
-    //     var bucketName = batchAddressBucket;
-    //     var fileName = fipsCode + '/input/' + files.file.originalFilename;
-    //     logger.info("putting file with name '" + fileName + "' into bucket '" + bucketName + "'");
-    //     s3.putObject({
-    //       Bucket: bucketName,
-    //       Key: fileName,
-    //       Body: fileStream
-    //     }, function (err) {
-    //       if (err) {
-    //         throw err;
-    //       } else {
-    //         queue.submitAddressFile(bucketName, fileName, fipsCode);
-    //       }
-    //     });
-    //   });
+      console.log(fields.fipsCode);
+      var date = fields["date"];
+      var fipsCode = fields["fipsCode"];
+      res.writeHead(200, {'content-type': 'text/plain'});
+      res.write('received upload:\n\n');
+      res.end(JSON.stringify(files));
+      var fileStream = fs.createReadStream(files.file.path);
+      fileStream.on('error', function (err) {
+        if (err) { throw err; }
+      });
+      fileStream.on('open', function () {
+        var s3 = new AWS.S3();
+        if (fipsCode === undefined) {
+          fipsCode = "undefined"
+        };
+        var fileName = fipsCode + '/' + date + '/' + files.file.originalFilename;
+        logger.info("putting file with name '" + fileName + "' into bucket '" + centralizationBucket + "'");
+        s3.putObject({
+          Bucket: centralizationBucket,
+          Key: fileName,
+          Body: fileStream
+        }, function (err) {
+          if (err) {
+            console.log("Error uploading centralization data file:");
+            console.log(err);
+            throw err;
+          }
+        });
+      });
     });
     return;
   }
