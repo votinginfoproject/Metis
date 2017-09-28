1955a8068c3625498802b9d05ab07d0c6c2f6e11
Troy Hill <troy@democracy.works>
2017-09-28 10:50:50 -0600

    Set up figwheel/nrepl support

--- project.clj
+++ project.clj
@@ -11,14 +11,19 @@
 
   :source-paths ["src/clj"]
 
+  :resource-paths ["resources" "."]
+
   :clean-targets ^{:protect false} ["public/assets/js/compiled" "target"]
 
-  :figwheel {:css-dirs ["resources/public/css"]}
+  :figwheel {:css-dirs ["resources/public/css"]
+             :http-server-root "public"}
 
   :profiles
   {:dev
-   {:dependencies [[binaryage/devtools "0.9.4"]]
-
+   {:dependencies [[binaryage/devtools "0.9.4"]
+                   [com.cemerick/piggieback "0.2.1"]
+                   [figwheel-sidecar "0.5.8"]]
+    :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
     :plugins      [[lein-figwheel "0.5.13"]]}}
 
 
@@ -26,12 +31,14 @@
   {:builds
    [{:id           "dev"
      :source-paths ["src/cljs"]
-     :figwheel     {:on-jsload "early-vote-site.core/mount-root"}
+     :figwheel     {:on-jsload "early-vote-site.core/mount-root"
+                    :autoload false}
      :compiler     {:main                 early-vote-site.core
                     :output-to            "public/assets/js/compiled/early_vote.js"
                     :output-dir           "public/assets/js/compiled/out"
                     :asset-path           "assets/js/compiled/out"
                     :source-map-timestamp true
+                    :optimizations        :none
                     :preloads             [devtools.preload]
                     :external-config      {:devtools/config {:features-to-install :all}}}}
 
