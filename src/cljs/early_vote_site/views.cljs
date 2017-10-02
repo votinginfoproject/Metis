760ed803a1c2a5e16f635bbeb7441cf8d5088d7a
Troy Hill <troy@democracy.works>
2017-10-02 08:23:52 -0600

    rework views so we can change pages easier

--- src/cljs/early_vote_site/views.cljs
+++ src/cljs/early_vote_site/views.cljs
@@ -1,11 +1,16 @@
 (ns early-vote-site.views
   (:require [re-frame.core :as re-frame]
-            [early-vote-site.election.views :as election]))
+            [early-vote-site.election.views :as election]
+            [early-vote-site.flash.views :as flash]))
 
 (defmulti panel identity)
 (defmethod panel :election/main [] [election/main-panel])
 
 (defn main-panel []
-  (let [active-panel (re-frame/subscribe :active-panel)]
+  (let [active-panel (re-frame/subscribe [:active-panel])]
     (fn []
-      (panel @active-panel))))
+      [:div
+       [:div "Active panel: " (pr-str @active-panel)]
+       (flash/message)
+       (flash/error)
+       (panel :election/main)])))
