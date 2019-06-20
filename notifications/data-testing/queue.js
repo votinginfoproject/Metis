4308cb185ebba0cf746536e5d9cbb09c5f5760ee
Troy Hill <troy@democracy.works>
2019-06-20 14:00:20 -0600

    adjust message parsing

--- notifications/data-testing/queue.js
+++ notifications/data-testing/queue.js
@@ -6,13 +6,13 @@ var sender = require('./../sender');
 module.exports = {
   processSuccessMessage: function(message) {
     logger.info("Received raw message: " + JSON.stringify(message));
-    var rabbitMessage = edn.toJS(edn.parse(JSON.parse(message.Body)["Message"]));
+    var rabbitMessage = edn.toJS(edn.parse(JSON.parse(message.Body)));
     logger.info("Message converted: " + JSON.stringify(rabbitMessage));
     sender.sendAddressTestSuccessNotifications(rabbitMessage, 'processedFeed');
   },
   processFailureMessage: function(message) {
     logger.info("Received raw message: " + JSON.stringify(message));
-    var rabbitMessage = edn.toJS(edn.parse(JSON.parse(message.Body)["Message"]));
+    var rabbitMessage = edn.toJS(edn.parse(JSON.parse(message.Body)));
     logger.info("Message converted: " + JSON.stringify(rabbitMessage));
     sender.sendAddressTestFailureNotifications(rabbitMessage, 'processedFeed');
   }
