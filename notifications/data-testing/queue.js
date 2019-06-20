503b1397f6f966645e63e804fc366f1a81536d7e
Troy Hill <troy@democracy.works>
2019-06-20 08:51:01 -0600

    More logging when message received

--- notifications/data-testing/queue.js
+++ notifications/data-testing/queue.js
@@ -5,13 +5,15 @@ var sender = require('./../sender');
 
 module.exports = {
   processSuccessMessage: function(message) {
+    logger.info("Received raw message: " + JSON.stringify(message));
     var rabbitMessage = edn.toJS(edn.parse(JSON.parse(message.Body)["Message"]));
-    logger.info("Received: " + JSON.stringify(rabbitMessage));
+    logger.info("Message converted: " + JSON.stringify(rabbitMessage));
     sender.sendAddressTestSuccessNotifications(rabbitMessage, 'processedFeed');
   },
   processFailureMessage: function(message) {
+    logger.info("Received raw message: " + JSON.stringify(message));
     var rabbitMessage = edn.toJS(edn.parse(JSON.parse(message.Body)["Message"]));
-    logger.info("Received: " + JSON.stringify(rabbitMessage));
+    logger.info("Message converted: " + JSON.stringify(rabbitMessage));
     sender.sendAddressTestFailureNotifications(rabbitMessage, 'processedFeed');
   }
 }
