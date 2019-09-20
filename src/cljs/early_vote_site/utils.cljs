c4e6fd5e4767a96ddb2dc249d8f17b1be9cc90fc
Liz Starin <liz@democracy.works>
2019-09-20 12:17:13 -0400

    formats times in view, but in UTC

--- src/cljs/early_vote_site/utils.cljs
+++ src/cljs/early_vote_site/utils.cljs
@@ -2,11 +2,13 @@
   (:refer-clojure :exclude [range iterate format max min])
   (:require [cljs-time.format :as format]
     [cljs-time.coerce :as coerce]
+    [cljs-time.local :as local]
     [early-vote-site.constants :as constants]))
 
 (def full-format (format/formatters :date-time))
 (def full-format-no-ms (format/formatters :date-time-no-ms))
 (def short-format (format/formatters :date))
+(def time-format (format/formatters :hour-minute))
 
 (defn format-date-string
   "Translates from a long date string format (ie 2017-10-10T13:23:34.00Z)
@@ -31,6 +33,15 @@
                  (format/parse full-format date-str)
                  (catch :default e (format/parse full-format-no-ms date-str)))]
     (coerce/to-date parsed)))
+
+(defn format-time-string
+  "Translates from a long date string format (ie 2017-10-10T13:23:34.00Z)
+   to a short format (12:45)."
+  [date-str]
+  (let [parsed (try
+                 (format/parse full-format date-str)
+                 (catch :default e (format/parse full-format-no-ms date-str)))]
+    (local/format-local-time parsed :hour-minute)))
 
 (defn flash-error-with-results
   [message]
